b50918154ad23ae07af391f5346e7b62d4fed5df
yulyen <kiseryota023@gmail.com>
2020-06-09 10:01:10 +0800

    added resources; updated design

--- certgen/admin.py
+++ certgen/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.db import models
-from .models import Tutorial
+from .models import Tutorial, TutorialSeries, TutorialCategory
 from tinymce.widgets import TinyMCE
 
 # Register your models here.
 class TutorialAdmin(admin.ModelAdmin):
     fieldsets = [
         ("Title/date", {'fields': ["title", "published"]}),
+        ("URL", {'fields': ["tutorial_slug"]}),
+        ("Series", {'fields': ["tutorial_series"]}),
         ("Content", {'fields': ["content"]}),
     ]
 
@@ -14,4 +16,6 @@ class TutorialAdmin(admin.ModelAdmin):
         models.TextField: {'widget': TinyMCE()},
     }
 
+admin.site.register(TutorialSeries)
+admin.site.register(TutorialCategory)
 admin.site.register(Tutorial, TutorialAdmin)
